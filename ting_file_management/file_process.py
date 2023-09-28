6929b66b1f1f8699625146935dac18a0a47802ae
Vinícius Souza <tradutorvinicius@gmail.com>
2023-09-28 16:05:23 -0300

    Resolve requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in list(instance.data):
+        if i["nome_do_arquivo"] == path_file:
+            return None
+    fileTxt = txt_importer(path_file)
+
+    dataObj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(fileTxt),
+        "linhas_do_arquivo": fileTxt
+    }
+
+    instance.enqueue(dataObj)
+    sys.stdout.write(str(dataObj))
 
 
 def remove(instance):
