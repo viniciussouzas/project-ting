eb53f1232bcb0ebf8f65ea68534560ab4a7993ea
Vinícius Souza <tradutorvinicius@gmail.com>
2023-10-02 15:45:57 -0300

    Resolve requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in instance.data:
+        ocorrencias = []
+
+        for index, line in enumerate(i["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                ocorrencias.append({"linha": index})
+    if ocorrencias:
+        result.append({
+            "palavra": word,
+            "arquivo": i["nome_do_arquivo"],
+            "ocorrencias": ocorrencias
+        })
+
+    return result
 
 
 def search_by_word(word, instance):
